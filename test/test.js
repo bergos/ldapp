879725ca5606896bc8ea088904e2fbfac1f5b374
bergi <bergi@axolotlfarm.org>
2015-01-08 13:59:26 +0100

    test code cleanup

--- test/test.js
+++ test/test.js
@@ -1,28 +1,31 @@
-var assert = require('assert');
-var fs = require('fs');
-var https = require('https');
-var url = require('url');
-//var request  = require('superagent');
+/* global describe:false, it:false */
+
+var
+  fs = require('fs'),
+  https = require('https'),
+  url = require('url');
 
 
-describe('LdApp', function() {
-	it('add webid', function(done) {
-		var options = url.parse('https://localhost:8443/card');
+describe('LdApp', function () {
+  it('add webid', function (done) {
+    var options = url.parse('https://localhost:8443/card');
 
-		options.method = 'PATCH';
-		options.headers = {"Content-Type": "text/turtle"};
-		options.key = fs.readFileSync('/home/bergi/webid.key').toString();
-		options.cert = fs.readFileSync('/home/bergi/webid.crt').toString();
-		options.rejectUnauthorized = false;
+    options.method = 'PATCH';
+    options.headers = {'Content-Type': 'text/turtle'};
+    options.key = fs.readFileSync('data/webid.key').toString();
+    options.cert = fs.readFileSync('data/webid.crt').toString();
+    options.rejectUnauthorized = false;
 
-		var req = https.request(options, function(res) {
-			console.log(res.statusCode);
-			done();
-		}).on('error', function(err) {
-			console.log(err);
-			done();
-		});
+    var req = https.request(options, function (res) {
+      console.log(res.statusCode);
 
-		req.end(fs.readFileSync(__dirname + '/support/add.webid.ttl').toString());
-	});
+      done();
+    }).on('error', function (err) {
+      console.log(err);
+
+      done();
+    });
+
+    req.end(fs.readFileSync(__dirname + '/support/add.webid.ttl').toString());
+  });
 });
