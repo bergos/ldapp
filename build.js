b4fcc018e5ca3ef4935ce151b6b8f1c5b732e6d4
bergi <bergi@axolotlfarm.org>
2015-04-04 01:22:55 +0200

    removed rdf-interfaces package from build script (now inc. in rdf-ext)

--- build.js
+++ build.js
@@ -4,6 +4,5 @@ var copy = function (source, target) {
   fs.writeFileSync(target, fs.readFileSync(source));
 };
 
-copy('node_modules/rdf-interfaces/rdfi.js', 'public/js/rdf.js');
 copy('node_modules/rdf-ext/dist/rdf-ext.js', 'public/js/rdf-ext.js');
 copy('node_modules/rdf-jsonify/rdf-jsonify.js', 'public/js/rdf-jsonify.js');
